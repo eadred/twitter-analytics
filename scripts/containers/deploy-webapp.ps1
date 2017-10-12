582c385adeaf4729de95bd7cfa76dc34ebf127f4
Eadred Birchenough <eadred.birchenough@zuhlke.com>
2017-10-12 10:29:28 +0100

    Added Kubernetes configuration files for webapp and couchbase services/deployments

--- scripts/containers/deploy-webapp.ps1
+++ scripts/containers/deploy-webapp.ps1
@@ -1,11 +1,23 @@
 gcloud container clusters get-credentials twitter-analytics --zone europe-west1-b --project analytics-174711
 
-kubectl run ta-webapp --image=eu.gcr.io/analytics-174711/ta-webapp --port=4567
+kubectl create -f couchbase-service.yaml
+kubectl create -f webapp-service.yaml
+kubectl create -f couchbase-deployment.yaml
+kubectl create -f webapp-deployment.yaml
 
-kubectl expose deployment ta-webapp --type="LoadBalancer"
 
-kubectl get service ta-webapp
+# kubectl expose deployment webapp-deployment --port=80 --target-port=4567 --type="LoadBalancer"
+
+# Get details
+# kubectl get deployment webapp-deployment
+# kubectl get pods -l app=webapp
+# kubectl get service webapp-service
+
+# Access shell on containers
+# kubectl exec $(kubectl get pods -l app=webapp -o jsonpath='{.items[*].metadata.name}') -c ta-webapp -it -- /bin/bash
+# kubectl exec $(kubectl get pods -l app=couchbase -o jsonpath='{.items[*].metadata.name}') -c ta-couchbase -it -- /bin/bash
 
 # To delete:
-# kubectl delete service ta-webapp
-# kubectl delete deployment ta-webapp
+# kubectl delete deployment webapp-deployment
+# kubectl delete deployment couchbase-deployment
+# kubectl delete service webapp-service
---
+++ scripts/containers/publish-couchbase-image.ps1
@@ -0,0 +1,3 @@
+docker tag ta-couchbase eu.gcr.io/analytics-174711/ta-couchbase
+
+gcloud docker -- push eu.gcr.io/analytics-174711/ta-couchbase
