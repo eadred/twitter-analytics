51e2dd4e9f0fd27fc4eb7262848f657febe01e7d
Eadred Birchenough <eadred.birchenough@zuhlke.com>
2017-10-13 08:08:05 +0100

    Container cluster now as 2 nodes

--- scripts/infrastructure/create-container-cluster.ps1
+++ scripts/infrastructure/create-container-cluster.ps1
@@ -10,7 +10,7 @@ clusters create $container_cluster `
 --image-type "COS" `
 --disk-size "10" `
 --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/bigquery","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/pubsub","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" `
---num-nodes "1" `
+--num-nodes "2" `
 --network "default" `
 --enable-cloud-logging `
 --no-enable-cloud-monitoring `
